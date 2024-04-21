eabe32929a31c009e3dd9712afaee2fdba950ede
Peter <leander.peter@gmail.com>
2024-04-21 21:37:25 +0300

    Fix split counting

--- ft_split.c
+++ ft_split.c
@@ -15,11 +15,17 @@
 static int	count_splits(char const *s, char c)
 {
 	int	count;
+	int	len;
 	int	i;
 
 	count = 0;
 	i = 0;
-	while (i < (int)ft_strlen(s))
+	len = (int)ft_strlen(s);
+	if (len == 0)
+	       return (0);
+	if (s[i] != c && i == 0)
+		count++;
+	while (i < len - 1)
 	{
 		if (s[i] == c && s[i + 1] != c)
 			count++;
@@ -33,17 +39,18 @@ char	**ft_split(char const *s, char c)
 	int	start;
 	int	end;
 	int len;
+	char	**splits;
 
+	splits = malloc(sizeof(char *) * (count_splits(s, c) + 1));
 	len = (int)ft_strlen(s);
-	printf("splits in \"%s\": %d\n:", s, count_splits(s, c));
 	start = 0;
 	end = 0;
-	//while (end < len)
-	//{
-	//	if (s[end] == c)
-	//	{
-	//		
-	//	}
-	//}
+	while (end < len)
+	{
+		if (s[end] == c)
+		{
+
+		}
+	}
 	return (NULL);
 }
