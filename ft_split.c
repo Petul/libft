5288e7b623426282069410066cedef1c459aa2ab
Peter Leander <pleander@student.hive.fi>
2024-04-22 13:02:02 +0300

    Add ft_split

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: pleander <pleander@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/19 10:45:50 by pleander          #+#    #+#             */
-/*   Updated: 2024/04/19 12:56:06 by pleander         ###   ########.fr       */
+/*   Updated: 2024/04/22 13:01:32 by pleander         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
@@ -22,7 +22,7 @@ static int	count_splits(char const *s, char c)
 	i = 0;
 	len = (int)ft_strlen(s);
 	if (len == 0)
-	       return (0);
+		return (0);
 	if (s[i] != c && i == 0)
 		count++;
 	while (i < len - 1)
@@ -34,23 +34,43 @@ static int	count_splits(char const *s, char c)
 	return (count);
 }
 
-char	**ft_split(char const *s, char c)
+static void	do_splitting(char **splits, char *trimmed, char c, int n_splits)
 {
 	int	start;
 	int	end;
-	int len;
-	char	**splits;
+	int	len;
+	int	i;
 
-	splits = malloc(sizeof(char *) * (count_splits(s, c) + 1));
-	len = (int)ft_strlen(s);
-	start = 0;
+	len = (int)ft_strlen(trimmed);
 	end = 0;
-	while (end < len)
+	i = 0;
+	start = end;
+	while (n_splits - i > 0)
 	{
-		if (s[end] == c)
+		if (trimmed[end] == c || (trimmed[end] == 0))
 		{
-
+			splits[i] = ft_substr(trimmed, start, end - start);
+			i++;
+			while (trimmed[end] == c)
+				end++;
+			start = end;
 		}
+		end++;
 	}
-	return (NULL);
+}
+
+char	**ft_split(char const *s, char c)
+{
+	char	**splits;
+	char	*trimmed;
+	int		n_splits;
+
+	trimmed = ft_strtrim(s, &c);
+	n_splits = count_splits(trimmed, c);
+	splits = ft_calloc(n_splits + 1, sizeof(char *));
+	if (!splits)
+		return (NULL);
+	do_splitting(splits, trimmed, c, n_splits);
+	free(trimmed);
+	return (splits);
 }
--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: pleander <pleander@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/19 09:53:05 by pleander          #+#    #+#             */
-/*   Updated: 2024/04/19 10:38:47 by pleander         ###   ########.fr       */
+/*   Updated: 2024/04/22 11:54:19 by pleander         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
@@ -19,8 +19,10 @@ char	*ft_strtrim(char const *s1, char const *set)
 	start = 0;
 	while (ft_strchr(set, s1[start]) != NULL && s1[start])
 		start++;
-	end = start;
-	while (ft_strchr(set, s1[end]) == NULL)
-		end++;
-	return (ft_substr(s1, start, end - start));
+	end = ft_strlen(s1) - 1;
+	while (ft_strchr(set, s1[end]) != NULL)
+		end--;
+	if (start > end)
+		return (ft_substr(s1, start, 0));
+	return (ft_substr(s1, start, end - start + 1));
 }
